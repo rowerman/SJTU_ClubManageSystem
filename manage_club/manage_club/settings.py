7f87e70aa527f22b266751506a6aff23f9487c6f
Aries-441 <sjtu.liu.jj@gmail.com>
2023-10-14 13:38:46 +0800

    ljj-10-14

--- manage_club/manage_club/settings.py
+++ manage_club/manage_club/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = "manage_club.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        'NAME': 'manages',
+        'NAME': 'nis3368',
         'USER': 'root',
-        'PASSWORD':'hanwen',
+        'PASSWORD':'ljj0403!',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
